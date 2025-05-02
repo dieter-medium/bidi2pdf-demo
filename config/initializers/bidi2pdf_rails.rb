e08e329a15de42953ac375511df886ed0c3903f6
Dieter S. <101627195+dieter-medium@users.noreply.github.com>
2025-05-02 19:34:18 +0200

    feat: update Chrome session arguments for development
    Adjusts the Chrome session arguments to automatically open dev tools in development mode. The no-sandbox option is retained for Codespaces environments, ensuring compatibility across different setups.

--- config/initializers/bidi2pdf_rails.rb
+++ config/initializers/bidi2pdf_rails.rb
@@ -17,11 +17,12 @@ Bidi2pdfRails.configure do |config|
   # config.general_options.default_timeout = 10 # Default timeout for various Bidi commands
   # config.general_options.chrome_session_args = ["--disable-gpu", "--disable-popup-blocking", "--disable-hang-monitor"] # Chrome session arguments
 
-  if ENV["CODESPACES"] && ENV["CODESPACES"] == "true"
-    chrome_session_args = Bidi2pdf::Bidi::Session::DEFAULT_CHROME_ARGS.dup
-    chrome_session_args << "--no-sandbox"
-    config.general_options.chrome_session_args = chrome_session_args
-  end
+  chrome_session_args = Bidi2pdf::Bidi::Session::DEFAULT_CHROME_ARGS.dup
+
+  chrome_session_args << "--auto-open-devtools-for-tabs" if Rails.env.development?
+  chrome_session_args << "--no-sandbox" if ENV["CODESPACES"] && ENV["CODESPACES"] == "true"
+
+  config.general_options.chrome_session_args = chrome_session_args
 
   #
   # Chromedriver Settings (when chromedriver run within your app)
