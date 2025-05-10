1ed8b4124b59ec45b53c58748ac26f96baacb73e
Dieter S. <101627195+dieter-medium@users.noreply.github.com>
2025-05-10 20:12:36 +0200

    feat: add network settings methods for container
    Introduce methods to fetch container bridge and gateway IPs from network settings. This enhances the ability to manage and interact with container networking configurations.

--- spec/support/patch_testcontainer_hosts.rb
+++ spec/support/patch_testcontainer_hosts.rb
@@ -6,6 +6,18 @@ module PatchTestcontainerHosts
 
     opts.compact
   end
+
+  def container_bridge_ip
+    network_settings&.fetch("IPAddress")
+  end
+
+  def container_gateway_ip
+    network_settings&.fetch("Gateway")
+  end
+
+  def network_settings
+    @_container&.json&.dig("NetworkSettings", "Networks", @_network&.name || "bridge")
+  end
 end
 
 Testcontainers::DockerContainer.prepend(PatchTestcontainerHosts)
