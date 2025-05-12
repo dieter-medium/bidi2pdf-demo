949d1064a9f787055e6569da5371e247b28b89cf
Dieter S. <101627195+dieter-medium@users.noreply.github.com>
2025-05-12 08:20:07 +0200

    fix: update asset paths for consistency
    Corrects asset paths in various files to ensure they point to the correct vendor directory. This change improves the organization of asset files and resolves potential loading issues.

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,4 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin_all_from "app/javascript/handlers", under: "handlers"
 
-pin "pagedjs", to: "javascript/paged.esm-0.4.3.js"
+pin "pagedjs", to: "vendor/paged.esm-0.4.3.js"
